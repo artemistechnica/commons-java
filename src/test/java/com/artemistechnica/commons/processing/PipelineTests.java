c6a84db6fe68ffe2e423388a002a5c0a5e58bf85
nickchampagne <nick.champagne@protonmail.com>
2024-05-08 10:30:03 -0400

    Added pipeline tests.

---
+++ src/test/java/com/artemistechnica/commons/processing/PipelineTests.java
@@ -0,0 +1,59 @@
+package com.artemistechnica.commons.processing;
+
+import com.artemistechnica.commons.datatypes.Either;
+import com.artemistechnica.commons.datatypes.EitherE;
+import com.artemistechnica.commons.errors.SimpleError;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class PipelineTests implements Pipeline {
+
+    @Test
+    public void testSimplePipeline() {
+        int count = 100;
+        List<Function<Integer, EitherE<Integer>>> fns = IntStream.rangeClosed(1, count).boxed().toList()
+                .stream().map(i -> (Function<Integer, EitherE<Integer>>) integer -> EitherE.success(integer + 1)).toList();
+
+        Function<Integer, EitherE<Integer>>[] steps = fns.<Function<Integer, EitherE<Integer>>>toArray(new Function[0]);
+        Function<Integer, EitherE<PipelineResult.Materializer<Integer>>> pipelineFn = this.<Integer>pipeline(steps);
+        EitherE<Integer> result = pipelineFn.apply(0).flatMapE(mat -> mat.materialize(i -> i));
+        assert(result.isRight());
+        assert(result.right.isPresent());
+        assert(result.right.get() == count);
+    }
+
+    @Test
+    public void testSimplePipelineWithExtremeNumberOfSteps() {
+        long count = 10000000L;
+        List<Function<Long, EitherE<Long>>> fns = LongStream.rangeClosed(1, count).boxed().toList()
+                .stream().map(i -> (Function<Long, EitherE<Long>>) n -> EitherE.success(n + 1)).toList();
+
+        Function<Long, EitherE<Long>>[] steps = fns.<Function<Long, EitherE<Long>>>toArray(new Function[0]);
+        Function<Long, EitherE<PipelineResult.Materializer<Long>>> pipelineFn = this.<Long>pipeline(steps);
+        EitherE<Long> result = pipelineFn.apply(0L).flatMapE(mat -> mat.materialize(i -> i));
+        assert(result.isRight());
+        assert(result.right.isPresent());
+        assert(result.right.get() == count);
+    }
+
+    @Test
+    public void testSimplePipelineAsFunctionArgument() {
+        Function<String, EitherE<PipelineResult.Materializer<String>>> p0 = pipeline(
+                (String str) -> EitherE.success(String.format("Hello, %s!", str))
+        );
+
+        BiFunction<String, Function<String, EitherE<PipelineResult.Materializer<String>>>, EitherE<String>> fn = (String name, Function<String, EitherE<PipelineResult.Materializer<String>>> pipe) -> {
+            return pipe.apply(name).flatMapE(mat -> mat.materialize(str -> str));
+        };
+
+        Either<SimpleError, String> result = fn.apply("Nick", p0);
+
+        assert(result.isRight());
+        assert(result.right.get().equals("Hello, Nick!"));
+    }
+}
